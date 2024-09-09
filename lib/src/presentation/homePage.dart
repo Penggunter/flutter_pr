1f1122cdb0f3ce2d6f435b809da31d37dcf742b3
shevi <shev.igor2002@yandex.ru>
2024-09-09 11:18:29 +0300

    change icon button

--- lib/src/presentation/homePage.dart
+++ lib/src/presentation/homePage.dart
@@ -59,7 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.accessible_forward_sharp),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
